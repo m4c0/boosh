a3fa95fad639135a947ca123ab23493bf20182ec
Eduardo Costa <m4c0@users.noreply.github.com>
2025-03-23 20:52:42 +0000

    drawing a quad

--- poc.cpp
+++ poc.cpp
@@ -2,16 +2,48 @@
 #pragma leco add_shader "poc.vert"
 #pragma leco add_shader "poc.frag"
 
+import dotz;
 import voo;
 import vapp;
+
+using vtx = dotz::vec4;
+
+void map_buf(voo::h2l_buffer & buf) {
+  unsigned c {};
+  voo::memiter<vtx> m { buf.host_memory(), &c };
+}
 
 struct : public vapp {
   void run() {
     main_loop("poc-voo", [&](auto & dq, auto & sw) {
+      voo::h2l_buffer buf { dq.physical_device(), sizeof(vtx) * 6 };
+      map_buf(buf);
+
+      voo::one_quad quad { dq.physical_device() };
+
       auto pl = vee::create_pipeline_layout();
-      voo::one_quad_render oqr { "poc", &dq, *pl };
-      render_loop(dq, sw, [&](auto cb) {
-        oqr.run(cb, sw.extent());
+      auto gp = vee::create_graphics_pipeline({
+        .pipeline_layout = *pl,
+        .render_pass = dq.render_pass(),
+        .shaders {
+          voo::shader("poc.vert.spv").pipeline_vert_stage(),
+          voo::shader("poc.frag.spv").pipeline_frag_stage(),
+        },
+        .bindings { quad.vertex_input_bind() },
+        .attributes { quad.vertex_attribute(0) },
+      });
+
+      ots_loop(dq, sw, [&](auto cb) {
+        voo::cmd_render_pass rp {{
+          .command_buffer = cb,
+          .render_pass = dq.render_pass(),
+          .framebuffer = sw.framebuffer(),
+          .extent = sw.extent(),
+        }};
+        vee::cmd_set_viewport(cb, sw.extent());
+        vee::cmd_set_scissor(cb, sw.extent());
+        vee::cmd_bind_gr_pipeline(cb, *gp);
+        quad.run(cb, 0, 1);
       });
     });
   }
--- poc.vert
+++ poc.vert
@@ -1,3 +1,8 @@
 #version 450
 
-void main() {}
+layout(location = 0) in vec2 pos;
+layout(location = 1) in vec4 wpos;
+
+void main() {
+  gl_Position = vec4(pos, 0, 1);
+}
