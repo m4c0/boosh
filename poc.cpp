8d56c4fa3407a1e4233085743618493e45a0412b
Eduardo Costa <m4c0@users.noreply.github.com>
2025-03-23 16:40:28 +0000

    boilerplate

---
+++ poc.cpp
@@ -0,0 +1,18 @@
+#pragma leco app
+#pragma leco add_shader "poc.vert"
+#pragma leco add_shader "poc.frag"
+
+import voo;
+import vapp;
+
+struct : public vapp {
+  void run() {
+    main_loop("poc-voo", [&](auto & dq, auto & sw) {
+      auto pl = vee::create_pipeline_layout();
+      voo::one_quad_render oqr { "poc", &dq, *pl };
+      render_loop(dq, sw, [&](auto cb) {
+        oqr.run(cb, sw.extent());
+      });
+    });
+  }
+} t;
