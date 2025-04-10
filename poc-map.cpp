56c450fd070f231881f789380b455305d3a8b970
Eduardo Costa <m4c0@users.noreply.github.com>
2025-04-10 12:16:43 +0100

    map: super-basic state machine

--- poc-map.cpp
+++ poc-map.cpp
@@ -1,4 +1,21 @@
 #pragma leco tool
 
+import jojo;
+import jute;
+import print;
+
+static void (*g_liner)(jute::view);
+
+static void error(jute::view line) {
+  die("invalid line: [", line, "]");
+}
+
+static void take_command(jute::view line) {
+  putln(line);
+  g_liner = error;
+}
+
 int main() {
+  g_liner = take_command;
+  jojo::readlines("example.map", [](auto line) { g_liner(line); });
 }
