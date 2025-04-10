4381164acc9598f34d5215707a2619dd86d55778
Eduardo Costa <m4c0@users.noreply.github.com>
2025-04-10 12:24:41 +0100

    map: parse/validate version

--- poc-map.cpp
+++ poc-map.cpp
@@ -8,14 +8,22 @@ static void (*g_liner)(jute::view);
 static unsigned g_line_number;
 static jute::view g_filename;
 
-static void error(jute::view line) {
-  die(g_filename, ":", g_line_number, ": invalid line: [", line, "]");
+static void error(jute::view msg, auto arg) {
+  die(g_filename, ":", g_line_number, ": ", msg, ": [", arg, "]");
+}
+
+static void check_version(jute::view arg) {
+  arg = arg.trim();
+  if (arg != "0") error("invalid version", arg);
 }
 
 static void take_command(jute::view line) {
   if (line == "") return;
-  putln(line);
-  g_liner = error;
+  
+  auto [cmd, args] = line.split(' ');
+  if (cmd == "version") return check_version(args);
+
+  error("unknown command", cmd);
 }
 
 int main() {
@@ -23,7 +31,7 @@ int main() {
   g_liner = take_command;
   g_line_number = 1;
   jojo::readlines(g_filename, [](auto line) {
-    g_liner(line);
+    g_liner(line.trim());
     g_line_number++;
   });
 }
