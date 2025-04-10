4c4fc7380e4bb8b14179fc06d2df5783348f4a34
Eduardo Costa <m4c0@users.noreply.github.com>
2025-04-10 12:19:22 +0100

    map: filename/line when failing
    It's a text file after all

--- poc-map.cpp
+++ poc-map.cpp
@@ -5,17 +5,25 @@ import jute;
 import print;
 
 static void (*g_liner)(jute::view);
+static unsigned g_line_number;
+static jute::view g_filename;
 
 static void error(jute::view line) {
-  die("invalid line: [", line, "]");
+  die(g_filename, ":", g_line_number, ": invalid line: [", line, "]");
 }
 
 static void take_command(jute::view line) {
+  if (line == "") return;
   putln(line);
   g_liner = error;
 }
 
 int main() {
+  g_filename = "example.map";
   g_liner = take_command;
-  jojo::readlines("example.map", [](auto line) { g_liner(line); });
+  g_line_number = 1;
+  jojo::readlines(g_filename, [](auto line) {
+    g_liner(line);
+    g_line_number++;
+  });
 }
