e9f8cce86a4bac834b7471e6e1f6695ff5eb761a
Eduardo Costa <m4c0@users.noreply.github.com>
2025-04-12 15:06:07 +0100

    poc-map: evolving into a map validator

--- poc-map.cpp
+++ poc-map.cpp
@@ -10,16 +10,7 @@ constexpr const jute::view filename = "example.map";
 int main() try {
   mapper::loader { filename };
 
-  for (auto y = 0; y < 32; y++) {
-    for (auto x = 0; x < 64; x++) {
-      switch (mapper::tiles[y][x]) {
-        case mapper::tile::empty: put(" "); break;
-        case mapper::tile::hall:  put("."); break;
-        case mapper::tile::wall:  put("X"); break;
-      }
-    }
-    putln();
-  }
+  putln("map seems fine");
 } catch (const mapper::loader::error & e) {
   err(filename, ":", e.line_number, ": ", e.msg);
   return 1;
