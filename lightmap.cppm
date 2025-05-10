cc299f8e55d7399268879a389c7ea2d5c260e169
Eduardo Costa <m4c0@users.noreply.github.com>
2025-05-10 17:54:52 +0100

    lightmap: preparing module

---
+++ lightmap.cppm
@@ -0,0 +1,16 @@
+export module lightmap;
+import v;
+
+namespace lightmap {
+  export class texture {
+    voo::offscreen::colour_buffer m_img;
+
+  public:
+    explicit texture(voo::device_and_queue * dq)
+      : m_img { dq->physical_device(), { 1024, 1024 }, VK_FORMAT_R8G8B8A8_UNORM }
+    {}
+
+    [[nodiscard]] auto image_view() const { return m_img.image_view(); }
+  };
+}
+
--- poc-lightmap.cpp
+++ poc-lightmap.cpp
@@ -2,6 +2,7 @@
 #pragma leco add_shader "poc-lightmap.frag"
 #pragma leco add_shader "poc-lightmap.vert"
 
+import lightmap;
 import vee;
 import voo;
 import vapp;
@@ -9,7 +10,13 @@ import vapp;
 struct app : public vapp {
   void run() override {
     main_loop("poc-lightmap", [&](auto & dq, auto & sw) {
+      lightmap::texture txt { &dq };
+
+      auto smp = vee::create_sampler(vee::linear_sampler);
+
       voo::single_frag_dset ds { 1 };
+      vee::update_descriptor_set(ds.descriptor_set(), 0, txt.image_view(), *smp);
+
       auto pl = vee::create_pipeline_layout(ds.descriptor_set_layout());
       voo::one_quad_render oqr { "poc-lightmap", &dq, *pl };
       extent_loop(dq.queue(), sw, [&] {
